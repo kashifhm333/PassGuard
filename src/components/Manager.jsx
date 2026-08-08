235c574963d72c5ce34569efb89071cc553b4c56
kashifhm333 <Kashifjaanmemon786110@gmail.com>
2026-08-08 11:58:09 +0500

    changing the color theme

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -145,27 +145,27 @@ const Manager = () => {
             <div className="absolute inset-0 -z-10 min-h-screen w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div>
             <div className="mx-auto w-full max-w-6xl px-4 pb-28 pt-4 sm:px-8 lg:px-12">
                 <h1 className='text-2xl font-bold text-center'>
-                    <span className='text-green-700'>&lt;</span>
-                    Pass
-                    <span className='text-green-700'>Guard/&gt;</span>
+                    <span className='text-purple-800'>&lt;</span>
+                    <span className='font-bold text-sky-950'>Pass</span>
+                    <span className='text-purple-800'>Guard/&gt;</span>
                 </h1>
-                <p className='text-green-900 text-lg text-center'>Your Own Password Manager</p>
+                <p className='text-purple-900 text-lg text-center'>Your Own Password Manager</p>
 
                 <div className='flex w-full flex-col gap-6 p-4 text-white sm:gap-8'>
-                    <input value={form.site} onChange={handleChange} placeholder='Enter Your Website URL' type="text" name='site' id='' className='rounded-full border border-green-500 w-full text-black p-4 py-1' />
+                    <input value={form.site} onChange={handleChange} placeholder='Enter Your Website URL' type="text" name='site' id='' className='rounded-full border bg-purple-100-500 w-full text-black p-4 py-1' />
 
                     <div className="flex w-full flex-col gap-4 md:flex-row md:gap-8">
-                        <input value={form.username} onChange={handleChange} placeholder='Enter Your Username' type="text" name='username' id='' className='rounded-full border border-green-500 w-full text-black p-4 py-1 md:w-1/2' />
+                        <input value={form.username} onChange={handleChange} placeholder='Enter Your Username' type="text" name='username' id='' className='rounded-full border bg-purple-100 w-full text-black p-4 py-1 md:w-1/2' />
 
                         <div className="relative w-full md:w-1/2">
-                            <input ref={passwordRef} value={form.password} onChange={handleChange} placeholder='Enter Your Password' type="password" name='password' id='' className='rounded-full border border-green-500 w-full text-black p-4 py-1' />
+                            <input ref={passwordRef} value={form.password} onChange={handleChange} placeholder='Enter Your Password' type="password" name='password' id='' className='rounded-full border bg-purple-100 w-full text-black p-4 py-1' />
                             <span className="absolute right-3 top-1/2 -translate-y-1/2 transform cursor-pointer text-gray-500" onClick={showPassword}>
                                 <img ref={ref} className='p-1 cursor-pointer' width="25" src="https://cdn-icons-png.flaticon.com/512/159/159604.png" alt="" />
                             </span>
                         </div>
                     </div>
                     <div className='flex justify-center gap-4 w-full'>
-                        <button onClick={savePassword} className='flex w-full items-center justify-center gap-2 rounded-full bg-green-500 py-2 font-bold text-white hover:bg-green-600 sm:w-1/2 md:w-1/3 lg:w-1/4 cursor-pointer'>
+                        <button onClick={savePassword} className='flex w-full items-center justify-center gap-2 rounded-full bg-purple-400 py-2 font-bold text-white hover:bg-purple-500 sm:w-1/2 md:w-1/3 lg:w-1/4 cursor-pointer'>
                             <lord-icon
                                 src="https://cdn.lordicon.com/vjgknpfx.json"
                                 trigger="hover"
@@ -182,10 +182,11 @@ const Manager = () => {
                 </div>
                 <div className="password px-4 pb-6">
                     <h2 className='text-lg font-semibold text-black'>Your Passwords</h2>
-                    {passwordArray.length === 0 && <p className='text-center text-green-900'>No Passwords Found</p>}
+                    {passwordArray.length === 0 && <p className='text-center bg-purple-400 text-black'>No Passwords Found</p>}
                    
-                    {passwordArray.length != 0 && <div className='mt-3 w-full overflow-x-auto rounded-md border border-green-500'><table className="min-w-160 table-auto w-full overflow-hidden">
-                        <thead className='bg-green-500 text-white' >
+                    {passwordArray.length != 0 && <div className='mt-3 w-full overflow-x-auto rounded-md border bg-purple-100'>
+                        <table className="min-w-160 table-auto w-full overflow-hidden">
+                        <thead className='bg-purple-400 text-white' >
                             <tr>
                                 <th className='py-2'>Site</th>
                                 <th className='py-2'>Username</th>
@@ -193,7 +194,7 @@ const Manager = () => {
                                 <th className='py-2'>Actions</th>
                             </tr>
                         </thead>
-                        <tbody className='bg-green-100'>
+                        <tbody className='bg-purple-200 text-black'>
                             {passwordArray.map((item, index) => {
                                 return <tr key={item.id || index}>
                                 <td className='w-32 px-2 py-2 text-center'><a className='break-all' href={item.site.startsWith('http') ? item.site : `https://${item.site}`} target="_blank" rel="noopener noreferrer">{item.site}</a></td>
@@ -207,11 +208,11 @@ const Manager = () => {
                                                 trigger="hover"
                                                 style={{ width: "25px", height: "25px" }}>
                                             </lord-icon>
-                                            
+                                           
                                         </span>
                                         <span className='cursor-pointer hover:scale-110 transition-transform p-1 flex items-center justify-center text-red-600' onClick={() => deletePassword(item.id)} title="Delete Password">
                                             <lord-icon
-                                                src="https://cdn.lordicon.com/gsqxdxog.json"
+                                                src="https://cdn.lordicon.com/gsqxdxog.json"        
                                                 trigger="hover"
                                                 style={{ width: "25px", height: "25px" }}>
                                             </lord-icon>
